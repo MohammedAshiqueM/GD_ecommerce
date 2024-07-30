301bf7db5f3112163e82f1dfd2a737c8a34a3a03
moh-ashique <mohammedashique942001@gmail.com>
2024-07-30 14:17:56 +0530

    static updated

--- Grand_depart/settings.py
+++ Grand_depart/settings.py
@@ -154,7 +154,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'staticfiles/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
